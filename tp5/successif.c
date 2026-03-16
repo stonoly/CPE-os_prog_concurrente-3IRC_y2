1e26691ca64cf422e480f9065f038b24ffa55f37
Pierre MOLY <pierre.moly@gmail.com>
2026-03-16 15:31:33 +0000

    Tp 5 - Ex 1 - Implement process creation and command execution using fork and exec in successif.c

--- tp5/successif.c
+++ tp5/successif.c
@@ -6,7 +6,41 @@
 
 #include "successif.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 
 int main() {
+    pid_t son = fork();
+    if (son == 0){
+
+        pid_t grand_son = fork();
+        if (grand_son == 0){
+
+            int error_gs = execlp("du","du","-sh","/usr/share", NULL);
+            if (error_gs == -1){
+                perror("exec fail");
+                exit(-1);
+            }  
+
+        }
+
+        wait(NULL);
+        int error_s = execlp("ls","ls","-l",NULL);
+        if (error_s == -1){
+            perror("exec fail");
+            exit(-1);
+        }
+    }
+
+    wait(NULL);
+    int error = execlp("ps","ps",NULL);
+    if (error == -1){
+        perror("exec fail");
+        exit(-1);
+    }
+
     return 0;
 }
