2303c7b9dde37019a7e06f8a310e796e56ff4205
Pierre MOLY <pierre.moly@gmail.com>
2026-03-18 18:17:16 +0000

    Tp 6 - Ex 2 - Implement pipe_simple functionality to process /etc/passwd using pipes and fork

--- tp6/pipe_simple.c
+++ tp6/pipe_simple.c
@@ -6,7 +6,39 @@
 
 #include "pipe_simple.h"
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 
 int main(){
+
+    int force[2];
+    int res = pipe(force);
+    if (res == -1) {
+        perror("Création Pipe");
+        exit(-1);
+    }
+
+    // Separation Anakin / Luke 
+    if (fork() == 0) {
+        // Luke
+        close(force[1]);
+
+        dup2(force[0],0);
+        close(force[0]);
+
+        execlp("tr", "tr", "a-z", "A-Z", NULL);
+
+    } else {
+        // Anakin
+        close(force[0]);
+
+        dup2(force[1],1);
+        close(force[1]);
+
+        execlp("cut", "cut", "-f", "1", "-d", ":", "/etc/passwd", NULL);
+    }
+
     return 0;
 }
