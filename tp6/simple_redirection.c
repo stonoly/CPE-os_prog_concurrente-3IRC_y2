67c6a33d36d4480d2d6d77f64a9a6af98c7467e5
Pierre MOLY <pierre.moly@gmail.com>
2026-03-18 16:45:49 +0000

    Tp 6 - Ex 2 - Implement simple redirection in simple_redirection.c to read /etc/passwd and count lines using wc

--- tp6/simple_redirection.c
+++ tp6/simple_redirection.c
@@ -6,7 +6,18 @@
 
 #include "simple_redirection.h"
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
 
 int main(){
+
+    int files = open("/etc/passwd",O_RDONLY);    
+    dup2(files,0);
+
+    execlp("wc","wc",NULL);
+
     return 0;
 }
