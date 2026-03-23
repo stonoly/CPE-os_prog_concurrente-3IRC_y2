360dbe6d2b2b9307c1d31e494df13ba7a953ad2e
Pierre MOLY <pierre.moly@gmail.com>
2026-03-23 14:07:05 +0000

    Tp 6 - Ex 3 - Add filters implementation with header and update Makefile and documentation for process management using pipes

---
+++ tp6/filters.c
@@ -0,0 +1,11 @@
+/* Fichier: filters.c
+* TP 6: Tubes Anonymes
+* auteur: Pierre MOLY
+* Exercice 3
+*/
+
+#include "filters.h"
+
+int main(){
+    return 0;
+}
---
+++ tp6/filters.h
@@ -0,0 +1,10 @@
+/* Fichier: filters.c
+* TP 6: Tubes Anonymes
+* auteur: Pierre MOLY
+* Exercice 3
+*/
+
+#ifndef FILTERS_H
+#define FILTERS_H
+
+#endif
