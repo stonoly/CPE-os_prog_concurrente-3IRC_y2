0be5ee4306041112239383f0e386c74cf81730b8
Pierre MOLY <pierre.moly@gmail.com>
2026-04-07 15:16:49 +0000

    Tp 9 - Ex 2 - Add signal handling in ignore.c to ignore SIGINT; create new ignore file and update Makefile accordingly.

--- tp9/ignore.c
+++ tp9/ignore.c
@@ -6,7 +6,24 @@
 
 #include "ignore.h"
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+
 
 int main(){
+    struct sigaction act;
+    memset(&act,0,sizeof(act));
+    act.sa_handler = SIG_IGN;
+    sigaction(SIGINT,&act,NULL);
+    
+    while(1 == 1){
+        puts("J'attends le Stop !!");
+        sleep(10);
+    }
+
+    puts("Là c'est la fin par épuisement");
     return 0;
 }
