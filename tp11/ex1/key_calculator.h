fe70cce32015c1b82491872369ae72a79e99c496
Pierre MOLY <pierre.moly@gmail.com>
2026-05-04 18:57:09 +0000

    Tp 11 - Ex 1 - Add new source files for TP11 - Producer/Consumer exercise, including client, server, clean, init, and their corresponding object files. Introduce key_calculator.h for key definitions. Update Makefile to include new targets.

---
+++ tp11/ex1/key_calculator.h
@@ -0,0 +1,21 @@
+/* Fichier: key_calculator.h
+* TP 11: Producer/Consumer
+* autor: Pierre MOLY
+* Exercice 1
+*/
+
+#ifndef KEY_CHANGE_H
+#define KEY_CHANGE_H
+
+#define KEY_TAB_INT 6278
+#define KEY_MUTEX_CAN_USE_TAB 63829
+
+#define KEY_MUTEX_P1 9302
+
+#define KEY_MUTEX_P2 5328
+
+#define KEY_SEM_P0 289302
+
+#define SIZE_TAB 3
+
+#endif
